b4367bd9f72b27664d9276ac97feb59f5ca35a26
nnastos <36231428+nnastos@users.noreply.github.com>
2025-05-04 23:45:03 +0300

    Login & Register
    Early controller implementations

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,12 @@
-﻿using Mango.Web.Models.Dto;
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mango.Web.Controllers
 {
@@ -16,15 +22,94 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Login()
         {
-            LoginRequestDto loginRequestDto = new ();
+            LoginRequestDto loginRequestDto = new();
 
-            return View(loginRequestDto);   
+            return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto? responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+
+                LoginResponseDto? loginResponse =
+                JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                return RedirectToAction("Index", "Home");
+
+
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
         }
 
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = SD.RoleAdmin,
+                    Value = SD.RoleAdmin
+                },
+                new SelectListItem
+                {
+                    Text = SD.RoleCustomer,
+                    Value = SD.RoleCustomer
+                }
+            };
+
+            ViewBag.RoleList = roleList;
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto? result = await _authService.RegisterAsync(obj);
+            ResponseDto? assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+
+                assignRole = await _authService.AssignRoleAsync(obj);
+
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = SD.RoleAdmin,
+                    Value = SD.RoleAdmin
+                },
+                new SelectListItem
+                {
+                    Text = SD.RoleCustomer,
+                    Value = SD.RoleCustomer
+                }
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
         }
 
         public IActionResult Logout()
--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -5,7 +5,8 @@
 
         public static string CouponAPIBase {  get; set; }
         public static string AuthAPIBase {  get; set; }
-
+        public static string RoleAdmin = "ADMIN";
+        public static string RoleCustomer = "CUSTOMER";
         public enum ApiType
         {
             GET,
