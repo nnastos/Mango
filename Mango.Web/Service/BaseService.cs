205434420ff87078da35ed2b25e88944ec31cbf8
nnastos <36231428+nnastos@users.noreply.github.com>
2025-05-27 22:16:24 +0300

    udemy end of lesson 67

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -9,13 +9,15 @@ namespace Mango.Web.Service
     public class BaseService : IBaseService
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ITokenProviderService _tokenProviderService;
 
-        public BaseService(IHttpClientFactory httpClientFactory) 
+        public BaseService(IHttpClientFactory httpClientFactory, ITokenProviderService tokenProviderService) 
         {
             _httpClientFactory = httpClientFactory;
+            _tokenProviderService = tokenProviderService;
         }
 
-        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true)
         {
             try
             {
@@ -23,6 +25,11 @@ namespace Mango.Web.Service
                 HttpRequestMessage message = new();
                 message.Headers.Add("Accept", "application/json");
                 // token
+                if (withBearer)
+                {
+                    var token = _tokenProviderService.GetToken();
+                    message.Headers.Add("Authorization", $"Bearer {token}"); // Add the token to the request header
+                }
 
                 message.RequestUri = new Uri(requestDto.Url);
                 if (requestDto.Data != null)
