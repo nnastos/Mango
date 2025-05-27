fcd5468b0667d8418069ac08ce13318dcaf6ecaf
nnastos <36231428+nnastos@users.noreply.github.com>
2025-05-27 22:22:48 +0300

    udemy end of lesson 69
    clean code in programm CouponAPI

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -53,30 +54,8 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-var settingsSection = builder.Configuration.GetSection("ApiSettings");
+builder.AddAppAuthentication();
 
-var secret = settingsSection.GetValue<string>("Secret");
-var issuer = settingsSection.GetValue<string>("Issuer");
-var audience = settingsSection.GetValue<string>("Audience");
-
-var key = Encoding.ASCII.GetBytes(secret);
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidIssuer = issuer,
-        ValidateAudience = true,
-        ValidAudience = audience
-    };
-});
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
