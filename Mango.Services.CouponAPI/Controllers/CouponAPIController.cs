095b0bd604d1eada22fb978a547bbc6027bc2fd5
nnastos <36231428+nnastos@users.noreply.github.com>
2025-05-27 22:28:54 +0300

    udemy end of lesson 70

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -78,7 +78,8 @@ namespace Mango.Services.CouponAPI.Controllers
             return _response;
         }
 
-        [HttpPost]        
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Post([FromBody] CouponDto couponDto)
         {
             try
@@ -98,6 +99,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpPut]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Put([FromBody] CouponDto couponDto)
         {
             try
@@ -118,6 +120,7 @@ namespace Mango.Services.CouponAPI.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Delete(int id)
         {
             try
